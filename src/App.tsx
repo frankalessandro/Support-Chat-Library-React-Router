3e1b35832b04850ceb6b472dfd2ce3c78c9850ea
Frank Alessandro <frank.alessandro.roldan@gmail.com>
2025-05-02 11:21:38 -0500

    Fin seccion 2 - esqueleto del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { AuthLayout } from "./auth/layout/AuthLayout";
+// import { AuthLayout } from "./auth/layout/AuthLayout";
+import ChatLayout from "./chat/layout/ChatLayout";
+import ChatPages from "./chat/pages/ChatPages";
 
 function App() {
   return (
     <>
-      <AuthLayout />
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPages/>
+      </ChatLayout>
     </>
   );
 }
